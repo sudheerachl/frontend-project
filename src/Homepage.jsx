f5c620879b9bf00dd261ef13f1a665ab5e9115d3
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-12 12:26:04 +0530

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <div className="HomePage">
       <h1>Home Page</h1>
       <div className="links">
-        <Link to="https://frontend-project-l030qlh04-cherukuri-lakshmi-sudheeras-projects.vercel.app/login-doctor">Login 1</Link>
-        <Link to="http://localhost:3001/login-user">Login 2</Link>
+        <Link to="/login-doctor">Login 1</Link>
+        <Link to="/login-user">Login 2</Link>
       </div>
     </div>
   );
