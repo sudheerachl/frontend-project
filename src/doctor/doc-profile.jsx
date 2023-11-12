7a9d59ce193aadfd5ce3ec37dc6590c410375da2
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-12 08:47:20 +0530

    Update doc-profile.jsx

--- src/doctor/doc-profile.jsx
+++ src/doctor/doc-profile.jsx
@@ -5,7 +5,7 @@ const Successdoctor = () => {
   return (
     <div >
         <h1>Login Success Page</h1>
-        <Link to='/login-doctor'>Logout</Link>
+        <Link to='/Homepage'>Logout</Link>
     </div>
   )
 }
