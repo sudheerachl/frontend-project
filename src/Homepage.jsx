a0fcb0130c3fa7d818683b46624c2181fd299f48
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-12 12:17:08 +0530

    Update Homepage.jsx

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -6,8 +6,8 @@ const HomePage = () => {
     <div className="HomePage">
       <h1>Home Page</h1>
       <div className="links">
-        <Link to="/login-doctor">Login 1</Link>
-        <Link to="/login-user">Login 2</Link>
+        <Link to="http://localhost:3001/login-doctor">Login 1</Link>
+        <Link to="http://localhost:3001/login-user">Login 2</Link>
       </div>
     </div>
   );
