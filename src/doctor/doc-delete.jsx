0e9c0204db72d5bd49c3530e032b7f88956b4b2e
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-18 09:16:50 +0530

    Update doc-delete.jsx

--- src/doctor/doc-delete.jsx
+++ src/doctor/doc-delete.jsx
@@ -16,12 +16,13 @@ const DeleteDoctor = () => {
         },
       });
 
-      if (response.data.status === 'SUCCESS' {
+      if (response.data.status === 'DOCTOR_NOT_FOUND') {
+        alert('Doctor not found!');}
+      else if (response.data.status === 'SUCCESS' {
         alert('Doctor deleted successfully!');
         setUsername('');
         setPassword('');
-      } else if (response.data.status === 'DOCTOR_NOT_FOUND') {
-        alert('Doctor not found!');
+      }
       } else if (response.data.status === 'INVALID_CREDENTIALS') {
         alert('Incorrect password or username. Please try again.');
       } else {
