3036c9777834ff55b6ae191b707e1dee9682f37f
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-18 16:19:27 +0530

    Update doc-login.jsx

--- src/doctor/doc-login.jsx
+++ src/doctor/doc-login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 import useToken from './useToken'; // Import the useToken hook
 
 const Logindoctor = () => {
