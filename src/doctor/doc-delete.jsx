ea3e1c02852d82d6f5d211d9a9bc51db7b458f42
sudheerachl <135159697+sudheerachl@users.noreply.github.com>
2023-11-18 08:44:40 +0530

    Update doc-delete.jsx

--- src/doctor/doc-delete.jsx
+++ src/doctor/doc-delete.jsx
@@ -7,30 +7,28 @@ const DeleteDoctor = () => {
 
 const handleSubmit = async (event) => {
   event.preventDefault();
+try {
+  const response = await axios.delete('https://backend-user-bms6.onrender.com/delete-doctor', {
+    data: {
+      username,
+      password,
+    },
+  });
 
-  try {
-    const response = await axios.delete('https://backend-user-bms6.onrender.com/delete-doctor', {
-      data: {
-        username,
-        password,
-      },
-    });
-
-    if (response.status === 200) {
-      alert('Doctor deleted successfully!');
-      setUsername('');
-      setPassword('');
-    } else if (response.status === 404) {
-      alert('Doctor not found!');
-    } else if (response.status === 422) {
-      alert('Unauthorized access. Please log in again.');
-    } else {
-      alert('Incorrect password or username. Please try again.');
-    }
-  } catch (error) {
-    console.error('Error deleting doctor:', error);
+  if (response.status === 200) {
+    alert('Doctor deleted successfully!');
+    setUsername('');
+    setPassword('');
+  } else if (response.status === 404) {
+    alert('Doctor not found!');
+  } else if (response.status === 422) {
+    alert('Unauthorized access. Please log in again.');
+  } else {
+    alert('Incorrect password or username. Please try again.');
   }
-};
+} catch (error) {
+  console.error('Error deleting doctor:', error);
+}};
 
 
 
